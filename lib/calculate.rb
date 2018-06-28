6337984d095f0ff43dcb4a311444233dee247dfa
mattb20 <matt_burgoyne@live.co.uk>
2018-06-28 18:56:36 +0100

    first test passed for order

--- lib/calculate.rb
+++ lib/calculate.rb
@@ -1,2 +1,18 @@
 class Calculate
+  def bubblesort(array)
+  array if array.length <= 1
+  swap = true
+  while swap
+    swap = false
+    (array.length - 1).times do |x| ## block iterates array.length - 1 times, passing in integers
+      ## from 0 to array.length - 1
+      if array[x] > array[x+1]
+        array[x], array[x+1] =  array[x+1], array[x]
+        swap = true
+      end
+      end
+  end
+  array
+
+end
 end
--- spec/calculate_spec.rb
+++ spec/calculate_spec.rb
@@ -2,6 +2,7 @@ require 'calculate'
 describe Calculate do
   subject(:array_numbers) { described_class.new }
   it 'will calculate the correct order using a sort algorithm' do
-    expect(array_numbers.bubblesort).to eq([1,2,3,3,4,4,5,6])
+    argument = [2,3,4,1,4,3,6,5]
+    expect(array_numbers.bubblesort(argument)).to eq([1,2,3,3,4,4,5,6])
   end
 end
